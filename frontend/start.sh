0e819b6db881e5367af28ba372f9904300e3892f
Dragotoniu.Cr.Corina@student.utcluj.ro <Dragotoniu.Cr.Corina@student.utcluj.ro>
2025-11-02 20:02:03 +0200

    new try

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if [ ! -d ".venv" ]; then
+VENV_PATH="/home/site/venv"
+
+if [ ! -d "$VENV_PATH" ]; then
   echo "Creating virtual environment..."
-  python3 -m venv .venv
-  source .venv/bin/activate
+  python3 -m venv $VENV_PATH
+  source $VENV_PATH/bin/activate
   pip install --upgrade pip
   pip install -r requirements.txt
 else
   echo "Using existing virtual environment..."
-  source .venv/bin/activate
+  source $VENV_PATH/bin/activate
 fi
 
 # start Streamlit
