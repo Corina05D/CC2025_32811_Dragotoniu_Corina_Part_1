0099444498461a0e98ff2536e72a127990912459
Dragotoniu.Cr.Corina@student.utcluj.ro <Dragotoniu.Cr.Corina@student.utcluj.ro>
2025-11-02 19:08:49 +0200

    test 2

--- backend/start.sh
+++ backend/start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# intră în directorul backend
-cd backend
+#cd backend
 
-# instalează dependențele (într-un virtual environment dacă vrei)
+# instrall dependencies
 pip install -r requirements.txt
 
-# pornește aplicația FastAPI cu gunicorn + uvicorn
+# start application FastAPI with gunicorn + uvicorn
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
--- frontend/start.sh
+++ frontend/start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# intră în folderul frontend
-cd frontend
+#cd frontend
 
-# instalează toate dependențele
+# install dependencies
 pip install -r requirements.txt
 
-# pornește aplicația Streamlit
+# start Streamlit
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
