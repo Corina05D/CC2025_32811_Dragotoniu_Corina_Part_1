0b3d09f8d0dac1f2130a6ddd8bc2840aba0d9e7e
Dragotoniu.Cr.Corina@student.utcluj.ro <Dragotoniu.Cr.Corina@student.utcluj.ro>
2025-11-02 20:11:23 +0200

    old version which is functional

--- backend/start.sh
+++ backend/start.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-VENV_PATH="/home/site/venv"
-
-if [ ! -d "$VENV_PATH" ]; then
-  echo "Creating virtual environment..."
-  python3 -m venv $VENV_PATH
-  source $VENV_PATH/bin/activate
-  pip install --upgrade pip
-  pip install -r requirements.txt
-else
-  echo "Using existing virtual environment..."
-  source $VENV_PATH/bin/activate
-fi
+pip install -r requirements.txt
 
 # start application FastAPI with gunicorn + uvicorn
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
--- frontend/start.sh
+++ frontend/start.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-VENV_PATH="/home/site/venv"
-
-if [ ! -d "$VENV_PATH" ]; then
-  echo "Creating virtual environment..."
-  python3 -m venv $VENV_PATH
-  source $VENV_PATH/bin/activate
-  pip install --upgrade pip
-  pip install -r requirements.txt
-else
-  echo "Using existing virtual environment..."
-  source $VENV_PATH/bin/activate
-fi
+pip install -r requirements.txt
 
 # start Streamlit
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
