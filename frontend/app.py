f4990d4930a4cd9df1c7494eb3f50ac069718330
Dragotoniu.Cr.Corina@student.utcluj.ro <Dragotoniu.Cr.Corina@student.utcluj.ro>
2025-11-02 18:44:48 +0200

    frontend API changed

--- frontend/app.py
+++ frontend/app.py
@@ -3,9 +3,12 @@ import requests
 
 st.title("Frontend Streamlit")
 
+backend_url = "https://cc2025-backend-b0cca9b2dheje3cq.westeurope-01.azurewebsites.net/api/data"
+
 try:
-    response = requests.get("http://127.0.0.1:8000/api/data")
+    response = requests.get(backend_url)
+    response.raise_for_status()
     data = response.json()
     st.success(f"Mesaj de la backend: {data['message']}")
-except Exception as e:
-    st.error("Nu s-a putut conecta la backend")
+except requests.exceptions.RequestException as e:
+    st.error(f"Nu s-a putut conecta la backend: {e}")
